9ed1c16ff18d6527fb4400cde490f76cf36d7aa4
Chad Jay Harrington <MistahWrite@gmail.com>
2021-05-04 19:31:43 -0600

    Got all the albums plus album art

--- discography.js
+++ discography.js
@@ -42,7 +42,8 @@ function getSearch() {
             axios.get(url)
             .then(function (response){
                 console.log(response.data);
-                discography.innerHTML += "<br><div class='biography'>";
+                var idArtist = response.data.artists[0].idArtist;
+                discography.innerHTML = "<br><div class='biography'>";
                 discography.innerHTML +="<h2>Biography:</h2> " + response.data.artists[0].strBiographyEN + "<BR/>";
                 discography.innerHTML += "<br><b>Country:</b> " + response.data.artists[0].strCountry + "<br/>";
                 discography.innerHTML += "<b>Genre:</b> " + response.data.artists[0].strGenre + "<br/>";
@@ -53,16 +54,31 @@ function getSearch() {
                 discography.innerHTML += "<b>Website:</b> <a href='http://" + response.data.artists[0].strWebsite + "' target='_blank'>" + response.data.artists[0].strWebsite + "</a><br/>";
                 discography.innerHTML += "</div>";
                 discography.innerHTML += "</div>";
+
+                url = 'https://theaudiodb.com/api/v1/json/1/album.php?i=' + idArtist;
+                axios.get(url)
+                .then(function(response){
+                    console.log(response);
+                    for(var i = 0; i < response.data.album.length; i++) {
+                        discography.innerHTML += "<div class='album'><a target='_blank' href='https://www.youtube.com/results?search_query=" + search_box + " " + response.data.album[i].strAlbum + "'><img src='" + response.data.album[i].strAlbumThumb + "'><br/>" + response.data.album[i].strAlbum + "</a><br/>Releeased: " + response.data.album[i].intYearReleased + "</div>";
+                    }
+                })
+                .catch(function(err){
+                    console.log(err);
+                })
+                .then(function() {
+                    console.log(url);
+                });
             })
             .catch(function(err){
-                    console.log("Error: " + err);  
+                    console.log(err);  
             })
             .then(function(){
                 console.log(url);
             });
             //discgraphy.innerHTML = "";
             for(var i = 0; i < response.data.album.length; i++) {
-                discography.innerHTML += "<div class='album'><a target='_blank' href='https://www.youtube.com/results?search_query=" + search_box + " " + response.data.album[i].strAlbum + "'>" + response.data.album[i].strAlbum + "</a><br/>Releeased: " + response.data.album[i].intYearReleased + "</div>";
+             //   discography.innerHTML += "<div class='album'><a target='_blank' href='https://www.youtube.com/results?search_query=" + search_box + " " + response.data.album[i].strAlbum + "'>" + response.data.album[i].strAlbum + "</a><br/>Releeased: " + response.data.album[i].intYearReleased + "</div>";
             }
             //discgraphy.innerHTML += "";
         })
